05d5971f6ffff92642eda765712ddca496644f0c
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-10-27 22:02:08 +0200

    get

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -92,6 +92,6 @@ class FileStorage:
             obj = self.all(cls)
             for k in obj:
                 print(k)
-                print(cls.name)
+                print(cls.split('.')[2])
                 if k == cls.__name__ + '.' + str(id):
                     return obj[k]
