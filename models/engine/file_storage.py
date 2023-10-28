a9a2c76c4aee6da15e9e854227e8fa675f8bed6b
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-10-28 16:16:08 +0200

    get

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -90,6 +90,7 @@ class FileStorage:
         """
         if cls and id:
             obj = self.all(cls)
+            print(list(self.all(State).values())[0].id)
             for k in obj:
                 print(k)
                 print(str(cls).split("'")[1].split('.')[2] + '.' + str(id))
