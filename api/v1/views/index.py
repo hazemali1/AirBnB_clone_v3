05559043033c24a7623946e5dccb5f4d475709b0
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-10-28 18:23:40 +0200

    api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 from api.v1.views import app_views
 from flask import jsonify
+"""
+import flask
+"""
 
 
 @app_views.route('/status')
 def status():
+    """
+		status
+		"""
     return jsonify({"status": "OK"})
