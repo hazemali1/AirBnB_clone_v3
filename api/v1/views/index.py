f05e5263e4c2f2d4d52f153d6a318ae79f0b5096
beboo22 <mt557829@gmail.com>
2023-10-28 20:13:23 +0200

    taskk

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,15 +3,27 @@
 import app flask json
 """
 from api.v1.views import app_views
+from models import storage
 from flask import jsonify
 """
 import flask
 """
 
 
-@app_views.route('/status')
+@app_views.route('/status', strict_slashes=False)
 def status():
     """
     status
     """
-    return jsonify({"status": "OK"})
+    return jsonify(
+        {
+        "status": "OK"
+        }
+    )
+
+@app_views.route('/api/v1/stats', strict_slashes=False)
+def stats():
+    """
+    stats
+    """
+    return
