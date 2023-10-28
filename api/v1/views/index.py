0b62df70296165167df3a8e3a0b8a7d87f22225c
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-10-28 20:26:27 +0200

    api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,7 @@ import app flask json
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 """
 import flask
 """
@@ -15,3 +16,19 @@ def status():
     status
     """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats')
+def stats():
+    """
+    stats
+    """
+    dec = {}
+    classes = storage.class_dict
+    dec["amenities"] = storage.count(classes["Amenity"])
+    dec["cities"] = storage.count(classes["City"])
+    dec["places"] = storage.count(classes["Place"])
+    dec["reviews"] = storage.count(classes["Review"])
+    dec["states"] = storage.count(classes["State"])
+    dec["users"] = storage.count(classes["User"])
+    return jsonify(dec)
