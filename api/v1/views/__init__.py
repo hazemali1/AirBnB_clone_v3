933936f5dc655d552546f6131a1e044acfb352d1
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-10-28 18:32:09 +0200

    api

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,3 @@
-#!/usr/bin/python3
-"""
-init
-"""
 from flask import Blueprint
 
 
@@ -9,4 +5,3 @@ app_views = Blueprint('app_views', __name__ , url_prefix='/api/v1')
 
 
 from api.v1.views.index import *
-
