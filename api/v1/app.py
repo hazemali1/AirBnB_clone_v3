7abe23aa8da1f3ef0a9932bc4eec0fcdd70524ec
beboo22 <mt557829@gmail.com>
2023-10-28 20:56:24 +0200

    task

--- api/v1/app.py
+++ api/v1/app.py
@@ -34,9 +34,7 @@ def error():
 
 
 if __name__ == '__main__':
-    """
-    main
-    """
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', 5000))
+    port = os.getenv('HBNB_API_PORT', 5000)
+    
     app.run(host=host, port=port, threaded=True)
