f234f2427c4ac378f0eb7fab2895558a51770dbe
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-10-28 21:00:26 +0200

    api

--- api/v1/app.py
+++ api/v1/app.py
@@ -34,7 +34,13 @@ def error(error):
 
 
 if __name__ == '__main__':
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = os.getenv('HBNB_API_PORT', 5000)
-    
+    """
+    main
+    """
+    host = '0.0.0.0'
+    port = 5000
+    if os.getenv('HBNB_API_HOST'):
+        host = os.getenv('HBNB_API_HOST')
+    if os.getenv('HBNB_API_PORT'):
+        port = os.getenv('HBNB_API_PORT')
     app.run(host=host, port=port, threaded=True)
