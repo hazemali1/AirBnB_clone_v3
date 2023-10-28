24e3af3864576c93784aed2e844ae1456fe23e30
beboo22 <mt557829@gmail.com>
2023-10-28 20:53:15 +0200

    task

--- api/v1/app.py
+++ api/v1/app.py
@@ -29,10 +29,6 @@ if __name__ == '__main__':
     """
     main
     """
-    host = '0.0.0.0'
-    port = 5000
-    if os.getenv('HBNB_API_HOST'):
-        host = os.getenv('HBNB_API_HOST')
-    if os.getenv('HBNB_API_PORT'):
-        port = os.getenv('HBNB_API_PORT')
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', 5000))
     app.run(host=host, port=port, threaded=True)
