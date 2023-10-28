37646ac0946d609f18bc7adaf21423660ab2d12d
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-10-29 00:15:49 +0200

    api

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -3,7 +3,7 @@
 import app flask json
 """
 from api.v1.views import app_views
-from flask import jsonify, abort
+from flask import jsonify
 from models import storage
 from models.engine.file_storage import class_dict
 from models.state import State
@@ -32,7 +32,4 @@ def states_id(state_id=None):
     for state in states.values():
         if state.id == state_id:
             li.append(state.to_dict())
-    if len(li):
-        return jsonify(li)
-    else:
-        abort(404)
+    return jsonify(li)
