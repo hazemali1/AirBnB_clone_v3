7e6ad6af42c8571a121774eabb86c48919e2779b
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-10-28 19:23:13 +0200

    api

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,38 +1,36 @@
 #!/usr/bin/python3
-"""
-import app flask jsonify
+"""Flask server (variable app)
 """
 
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
+from os import getenv
 from api.v1.views import app_views
-import os
-"""
-flask
-"""
-
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def teardown(exception):
-    """
-    teardown app context
-    """
+def downtear(self):
+    '''Status of your API'''
     storage.close()
 
 
-if __name__ == '__main__':
-    """
-    main
-    """
-    host = '0.0.0.0'
-    port = 5000
-    if os.getenv('HBNB_API_HOST'):
-        host = os.getenv('HBNB_API_HOST')
-    if os.getenv('HBNB_API_PORT'):
-        port = os.getenv('HBNB_API_PORT')
+@app.errorhandler(404)
+def page_not_found(error):
+    '''return render_template'''
+    return jsonify('error='Not found'), 404
+
+
+if __name__ == "__main__":
+    host = getenv('HBNB_API_HOST')
+    port = getenv('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
     app.run(host=host, port=port, threaded=True)
+		
