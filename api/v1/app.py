466a24ed574a3db4eb00507f05c4c788e2249525
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-10-28 19:20:18 +0200

    api

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,1 +1,38 @@
 #!/usr/bin/python3
+"""
+import app flask jsonify
+"""
+
+
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+import os
+"""
+flask
+"""
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """
+    teardown app context
+    """
+    storage.close()
+
+
+if __name__ == '__main__':
+    """
+    main
+    """
+    host = '0.0.0.0'
+    port = 5000
+    if os.getenv('HBNB_API_HOST'):
+        host = os.getenv('HBNB_API_HOST')
+    if os.getenv('HBNB_API_PORT'):
+        port = os.getenv('HBNB_API_PORT')
+    app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,17 +1,1 @@
 #!/usr/bin/python3
-"""
-import app flask jsonify
-"""
-from api.v1.views import app_views
-from flask import jsonify
-"""
-import flask
-"""
-
-
-@app_views.route('/status')
-def status():
-    """
-    status
-    """
-    return jsonify({"status": "OK"})
