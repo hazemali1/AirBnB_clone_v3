8f378288c9ca0973a4f3f1a1cfb6f3353c67f73d
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-10-29 20:20:19 +0200

    api

--- api/v1/app.py
+++ api/v1/app.py
@@ -17,7 +17,7 @@ flask
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/*": {"origins": "*"}})
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def teardown(exception):
