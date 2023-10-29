05b3984f8053efa6effe18e795ac126beb461749
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-10-29 16:31:48 +0200

    api

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -35,7 +35,7 @@ def states():
         return (jsonify(obj.to_dict()), 201)
 
 
-@app_views.route('/states/<state_id>', methods=['GET', 'DELETE'])
+@app_views.route('/states/<state_id>', methods=['GET', 'DELETE', 'PUT'])
 def states_id(state_id=None):
     """states_id"""
     state = storage.get(State, state_id)
@@ -47,4 +47,14 @@ def states_id(state_id=None):
         storage.delete(state)
         storage.save()
         return (jsonify({}), 200)
+    if request.method == 'PUT':
+        req = request.get_json()
+        if req is None:
+            abort(400, 'Not a JSON')
+        keys_to_ignore = ["id", "created_at", "updated_at"]
+        for key, value in req.items():
+            if key not in keys_to_ignore:
+                setattr(State, key, value)
+        storage.save()
+        return make_response(jsonify(state.to_dict()), 200)
 
