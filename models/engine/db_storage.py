93e17c22ca0ad490b05710e55abc34c59784bafc
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-10-29 19:30:04 +0200

    api

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -86,16 +86,7 @@ class DBStorage:
         """
         self.__session.remove()
 
-    def get(self, cls, id):
-        """
-        get
-        """
-        if cls and id:
-            obj = self.all(cls)
-            for k in obj:
-                if k == str(cls).split("'")[1].split('.')[2] + '.' + str(id):
-                    return obj[k]
-        return None
+
 
     def count(self, cls=None):
         """
