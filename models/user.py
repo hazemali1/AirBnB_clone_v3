b5c871f7b66eaba715aba24606be6245529371f2
beboo22 <mt557829@gmail.com>
2023-10-30 19:02:22 +0200

    task

--- models/user.py
+++ models/user.py
@@ -6,6 +6,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 import os
+from hashlib import md5
 
 
 class User(BaseModel, Base):
