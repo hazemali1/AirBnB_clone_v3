40632114644efb13303ad1f74bbf5b8630a91776
hazemali1 <124938032+hazemali1@users.noreply.github.com>
2023-11-03 15:29:09 +0200

    edit back

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -56,4 +56,5 @@ def states_id(state_id=None):
             if key not in keys_to_ignore:
                 setattr(State, key, value)
         storage.save()
+        print("hello")
         return (jsonify(state.to_dict()), 200)
